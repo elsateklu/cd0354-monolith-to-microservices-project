567b9876e4a0177b7c7be31191b1f7fc03f93323
elsateklu7 <elsateklu7@gmail.com>
2022-10-16 11:38:42 +0300

    new frontend version

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://a5b46598106d44693b285c84aa946d7d-1954504855.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
